c9daf6e1ba43389649397d093cb85a0789d0294d
Vo Phong <phongwind21092001@gmail.com>
2023-07-21 18:13:32 +0700

    add Navigation

---
+++ src/views/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import './Navigation.scss'
+
+const Navigation = () => {
+    return (
+        <div className="topnav">
+            <a className="active" href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+    )
+}
+export default Navigation
