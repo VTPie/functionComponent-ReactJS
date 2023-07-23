888cf50bab46d0d71ced942220acf488cec1a176
Vo Phong <phongwind21092001@gmail.com>
2023-07-23 15:41:18 +0700

    handle error

--- src/views/Population/Population.js
+++ src/views/Population/Population.js
@@ -6,16 +6,20 @@ const Population = () => {
     const [dataCity, setDataCity] = useState([])
     //Ban đầu - chưa có data nên 'loading = true'
     const [loading, setLoading] = useState(true)
+    const [errMessage, setErrMessage] = useState(false)
 
     //didMount
     useEffect(async () => {
-        //test delay 3s
-        setTimeout(async () => {
+        try {
             let res = await axios.get('https://countriesnow.space/api/v0.1/countries/population/cities')
-            let data = res && res.data.data ? res.data.data : []
+            let data = (res && res.data.data) ? res.data.data : []
             setDataCity(data)
             setLoading(false)
-        }, 3000)
+            setErrMessage(false)
+        } catch (e) {
+            setLoading(false)
+            setErrMessage(true)
+        }
     }, [])
 
     return (
@@ -29,7 +33,7 @@ const Population = () => {
                 </tr>
             </thead>
             <tbody>
-                {loading === false && dataCity && dataCity.length > 0 &&
+                {errMessage === false && loading === false && dataCity && dataCity.length > 0 &&
                     dataCity.map(item => {
                         return (
                             <tr key={`${item.city}.${2013}`}>
@@ -43,6 +47,11 @@ const Population = () => {
                 {loading === true &&
                     <tr>
                         <td colSpan='3' style={{ 'textAlign': 'center' }}>Loading...</td>
+                    </tr>
+                }
+                {errMessage === true &&
+                    <tr>
+                        <td colSpan='3' style={{ 'textAlign': 'center' }}>Something wrong ...</td>
                     </tr>
                 }
             </tbody>
